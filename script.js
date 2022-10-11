264ecd9d130bfc538c358db1ac670a9bccfcb1ad
kendrick Brown <chefken360@gmail.com>
2022-10-10 20:25:52 -0700

    javaduhhut

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ var savedSearches = [];
 
 var searchHistoryList = function(cityName) {
     $('.past-search:contains("' + cityName + '")').remove();
+}
+
+var searchHistoryEntry = $("<p>");
+    searchHistoryEntry.addClass("past-search");
+    searchHistoryEntry.text(cityName);
+
+    var searchEntryContainer = $("<div>");
+    searchEntryContainer.addClass("past-search-container");
+
+    searchEntryContainer.append(searchHistoryEntry);
