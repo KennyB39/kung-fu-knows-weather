6a5e0a7e4e798cb4d2000ba1c85d4fccb1921341
kendrick Brown <chefken360@gmail.com>
2022-10-12 07:23:24 -0700

    java the hut

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # kung-fu-knows-weather
+
+Create a few global variables
+
+Make variable "searchHistoryList" that will make list of previously searched cities
+
+Create container for entries 
+
+Append entry to container
+
+Append entry container to search history containe
+
--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var apiKey = "a2852dfc1aa75c0227e70b0469f64fe9";
 var savedSearches = [];
 
-var searchHistoryList = function(cityName) {
+var searchHistoryList = function (cityName) {
     $('.past-search:contains("' + cityName + '")').remove();
-}
+
 
     var searchHistoryEntry = $("<p>");
     searchHistoryEntry.addClass("past-search");
@@ -17,7 +17,7 @@ var searchHistoryList = function(cityName) {
     var searchHistoryContainerEl = $("#search-history-container");
     searchHistoryContainerEl.append(searchEntryContainer);
 
-    if (savedSearches.length > 0){
+    if (savedSearches.length > 0) {
         var previousSavedSearches = localStorage.getItem("savedSearches");
         savedSearches = JSON.parse(previousSavedSearches);
     }
@@ -27,7 +27,7 @@ var searchHistoryList = function(cityName) {
 
     $("#search-input").val("");
 
-    var loadSearchHistory = function() {
+    var loadSearchHistory = function () {
         var savedSearchHistory = localStorage.getItem("savedSearches");
         if (!savedSearchHistory) {
             return false;
@@ -39,18 +39,110 @@ var searchHistoryList = function(cityName) {
         }
     };
 
-    var currentWeatherSection = function(cityName) {
+    var currentWeatherSection = function (cityName) {
         fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`)
-        .then(function(response) {
-            return response.json();
-        })
+            .then(function (response) {
+                return response.json();
+            })
     }
-        .then(function(response) {
+        .then(function (response) {
             var cityLon = response.coord.lon;
             var cityLat = response.coord.lat;
-        })
+
             fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLon}&exclude=minutely,hourly,alerts&units=imperial&appid=${apiKey}`)
+                .then(function (response) {
+                    return response.json();
+                })
+
+                .then(function (response) {
+                    return response.json();
+                })
+
+                .then(function (response) {
+                    searchHistoryList(cityName);
+
+
+                    var currentWeatherContainer = $("#current-weather-container");
+                    currentWeatherContainer.addClass("current-weather-container");
+
+                    var currentTitle = $("#current-title");
+                    var currentDay = moment().format("M/D/YYYY");
+                    currentTitle.text(`${cityName} (${currentDay})`);
+                    var currentIcon = $("#current-weather-icon");
+                    currentIcon.addClass("current-weather-icon");
+                    var currentIconCode = response.current.weather[0].icon;
+                    currentIcon.attr("src", `https://openweathermap.org/img/wn/${currentIconCode}@2x.png`);
+
+                    var currentTemperature = $("#current-temperature");
+                    currentTemperature.text("Temperature: " + response.current.temp + " \u00B0F");
+
+                    var currentHumidity = $("#current-humidity");
+                    currentHumidity.text("Humidity: " + response.current.humidity + "%");
+
+                    var currentWindSpeed = $("#current-wind-speed");
+                    currentWindSpeed.text("Wind Speed: " + response.current.wind_speed + " MPH");
+
+                    var currentUvIndex = $("#current-uv-index");
+                    currentUvIndex.text("UV Index: ");
+                    var currentNumber = $("#current-number");
+                    currentNumber.text(response.current.uvi);
+
+                    if (response.current.uvi <= 2) {
+                        currentNumber.addClass("favorable");
+                    } else if (response.current.uvi >= 3 && response.current.uvi <= 7) {
+                        currentNumber.addClass("moderate");
+                    } else {
+                        currentNumber.addClass("severe");
+                    }
+                })
+        })
+
+        .catch(function (err) {
+            $("#search-input").val("");
+            alert("We could not find the city you searched for. Try searching for a valid city.");
+        });
+};
+
+var fiveDayForecastSection = function (cityName) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`)
+        .then(function (response) {
+            return response.json();
+        })
+
+        .then(function (response) {
+            var cityLon = response.coord.lon;
+            var cityLat = response.coord.lat;
+            fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${cityLat}&lon=${cityLon}&exclude=minutely,hourly,alerts&units=imperial&appid=${apiKey}`)
+             
+                
             .then(function(response) {
                 return response.json();
             })
-        
+            .then(function(response) {
+                console.log(response);
+                
+                var futureForecastTitle = $("#future-forecast-title");
+                futureForecastTitle.text("5-Day Forecast:")
+
+                for (var i = 1; i= 5; i+) {
+                    var futureCard = $(".future-card");
+                        futureCard.addClass("future-card-details");
+
+                        var futureDate = $("#future-date-" + i);
+                        date = moment().add(i, "d").format("M/D/YYYY");
+                        futureDate.text(date);
+
+                        var futureIcon = $("#future-icon-" + i);
+                        futureIcon.addClass("future-icon");
+                        var futureIconCode = response.daily[i].weather[0].icon;
+                        futureIcon.attr("src", `https://openweathermap.org/img/wn/${futureIconCode}@2x.png`);
+
+                        var futureTemp = $("#future-temp-" + i);
+                        futureTemp.text("Temp: " + response.daily[i].temp.day + " \u00B0F");
+
+                        var futureHumidity = $("#future-humidity-" + i);
+                        futureHumidity.text("Humidity: " + response.daily[i].humidity + "%");
+                    }
+                })
+        })
+};
